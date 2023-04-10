be14db157d1ae1dc5992045f5367f78dd4c83224
ZhiXuan Zhong <dooi921@gmail.com>
2023-04-10 16:55:24 +0800

    [week3] Finish Assignment2

--- week-3/app.js
+++ week-3/app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from 'express'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,6 +14,34 @@ app.listen(port, () => {
 })
 
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(  __dirname + '/index.html')
+})
+
+app.get('/getData', (req,res) => {
+
+    const urlNum = req.query.number
+
+    if (urlNum === undefined){
+        return res.send(`
+        <h1>Lack of Parameter</h1>
+        <p>Try <a href="?number=5">?number=5</a></p>
+        <p>Try <a href="?number=xyz">?number=xyz</a></p>
+        `)
+    }
+
+    if (isNaN(+urlNum)){
+        return res.send(`
+        <h1>Wrong Parameter</h1>
+        <p>Try <a href="?number=5">?number=5</a></p>
+        `)
+    }
+
+    let sum = 0
+    for (let i = 1; i <= urlNum; i++) {
+        sum += i
+    }
+    
+    res.send(`<h1>The result of your request is ${sum}<h1>`)
+
 })
