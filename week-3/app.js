3e47f0d885990eb1ffec543ab79986a0f845991a
ZhiXuan Zhong <dooi921@gmail.com>
2023-04-15 22:25:33 +0800

    [week3] Remove JSON.stringify before send

--- week-3/app.js
+++ week-3/app.js
@@ -4,51 +4,47 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 app.listen(port, () => {
-    console.log(`Server is Up and Runing on port ${port}`)
+  console.log(`Server is Up and Runing on port ${port}`)
 })
 
 app.use(express.static('public'))
 
-
 app.get('/', (req, res) => {
-    res.sendFile(  __dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/getData', (req,res) => {
+app.get('/getData', (req, res) => {
+  const urlNum = req.query.number
 
-    const urlNum = req.query.number
+  if (urlNum === undefined) {
+    return res.send({
+      title: 'Lack of Parameter',
+      desc: 'Enter a Positive integer',
+    })
+  }
 
-    if (urlNum === undefined){
-        return res.send(JSON.stringify({
-            title : "Lack of Parameter",
-            desc : "Enter a Positive integer"
-        }))
-    }
+  if (isNaN(+urlNum) || urlNum < 0) {
+    return res.send({
+      title: 'Wrong Parameter',
+      desc: `Positive integer Only! Your request is ${urlNum}`,
+    })
+  }
 
-    if (isNaN(+urlNum) || urlNum < 0){
-        return res.send(
-            JSON.stringify({
-            title : "Wrong Parameter",
-            desc : `Positive integer Only! Your request is ${urlNum}`
-        }))
-    }
+  let sum = 0
+  for (let i = 1; i <= urlNum; i++) {
+    sum += i
+  }
 
-    let sum = 0
-    for (let i = 1; i <= urlNum; i++) {
-        sum += i
-    }
-    
-    res.send(
-        JSON.stringify({
-        title : `The result is ${sum}`,
-        desc : `Base on your request: ${urlNum}`
-    }))
-
+  res.send(
+    JSON.stringify({
+      title: `The result is ${sum}`,
+      desc: `Base on your request: ${urlNum}`,
+    })
+  )
 })
--- week-3/public/sum.js
+++ week-3/public/sum.js
@@ -1,27 +1,26 @@
 async function handleClick() {
   try {
-    const sumOutput = document.getElementById("sumOutput").textContent;
-    const numInput = document.getElementById("numInput").value;
+    const sumOutput = document.getElementById('sumOutput').textContent
+    const numInput = document.getElementById('numInput').value
 
     async function getData(url) {
-      const response = await fetch(url);
-      return response.json();
+      const response = await fetch(url)
+      return response.json()
     }
 
     const result =
-      numInput === ""
-        ? await getData("/getData")
-        : await getData(`/getData?number=${numInput}`);
+      numInput === ''
+        ? await getData('/getData')
+        : await getData(`/getData?number=${numInput}`)
 
-    document.getElementById("numInput").value = "";
+    document.getElementById('numInput').value = ''
 
-    const { title, desc } = result;
-    document.getElementById("sumOutput").innerHTML = `
+    const { title, desc } = result
+    document.getElementById('sumOutput').innerHTML = `
         <h1>${title}</h1>
         <p>${desc}</p>
-        `;
-        
+        `
   } catch (error) {
-    console.error(`ERROR: ${error}`);
+    console.error(`ERROR: ${error}`)
   }
 }
