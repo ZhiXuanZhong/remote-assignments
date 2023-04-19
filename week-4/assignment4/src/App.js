ed129d4b0efa45f7b7123d08de17e790399874e6
ZhiXuan Zhong <dooi921@gmail.com>
2023-04-19 15:31:54 +0800

    [week4] Assignment 4 bug fixes

--- week-4/assignment4/src/App.js
+++ week-4/assignment4/src/App.js
@@ -1,36 +1,43 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import DataRow from './DataRow'
 import './styles.css'
 
-let page = 1
-
 export default function App() {
   const [repoList, setRepoList] = useState([])
-  const dataFetchedRef = useRef(false)
+  const [page, setPage] = useState(1)
 
   async function dataFetcher() {
-    const res = await fetch(
-      `https://api.github.com/orgs/facebook/repos?per_page=5&page=${page}`
-    )
-    const data = await res.json()
-    setRepoList((prevRepoList) => [...prevRepoList, ...data])
+    try {
+      const res = await fetch(
+        `https://api.github.com/orgs/facebook/repos?per_page=5&page=${page}`
+      )
+      const data = await res.json()
+
+      if (data.length === 0) {
+        alert('End of list')
+      } else {
+        setRepoList((prevRepoList) => [...prevRepoList, ...data])
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
+  
   useEffect(() => {
-    if (dataFetchedRef.current) return
-    dataFetchedRef.current = true
     dataFetcher()
+    setPage(2)
   }, [])
 
   function handleMore() {
     dataFetcher()
-    page++
+    setPage((prev) => prev + 1)
   }
 
   return (
     <div className="App">
       {repoList.map((item) => {
-        return <DataRow {...item} key={Math.random()} /> //這邊要用item.id都失敗不知道為何
+        return <DataRow {...item} key={item.id} />
       })}
       <h1 className="load-more" onClick={handleMore}>
         More
